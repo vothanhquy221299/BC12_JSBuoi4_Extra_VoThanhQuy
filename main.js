1ede81bb521273a92b32df8442d72593b97ac3ef
Vo Thanh Quy <55434741+vothanhquy221299@users.noreply.github.com>
2021-06-09 10:07:05 +0700

    next add

--- main.js
+++ main.js
@@ -2,16 +2,16 @@
  * Bai 1
  */
 
-var isLeapYear = function(year){
-    if((year % 4 == 0 && year % 100 ==0) || year % 400 == 0){
+var isLeapYear = function (year) {
+    if ((year % 4 == 0 && year % 100 == 0) || year % 400 == 0) {
         return true;
     }
     return false;
 }
 
-var calculatingDaysOfMonth = function(month, year){
+var calculatingDaysOfMonth = function (month, year) {
     var numOfDays;
-    switch(month){
+    switch (month) {
         case 1:
         case 3:
         case 5:
@@ -27,45 +27,43 @@ var calculatingDaysOfMonth = function(month, year){
         case 11:
             numOfDays = 30;
             break;
-        case 2: 
-            if(isLeapYear(year)){
+        case 2:
+            if (isLeapYear(year)) {
                 numOfDays = 29;
-            } else{
+            } else {
                 numOfDays = 28;
             }
-             break;   
+            break;
         default: 'This is not suitable!'
     }
     return numOfDays;
 }
-var re = calculatingDaysOfMonth(02,2020);
-console.log(re);
 
-var findPreviousDay = function(day, month, year){
+var findPreviousDay = function (day, month, year) {
     var tempPre = "";
-    if(day == 1){
+    if (day == 1) {
         day = calculatingDaysOfMonth(month, year);
-        if(month == 1){
+        if (month == 1) {
             month = 12;
             year--;
-        }else{
+        } else {
             month--;
         }
-    }else{
+    } else {
         day--;
     }
-     return temp = day + "/" + month + "/" + year
+    return temp = day + "/" + month + "/" + year
 }
 
-var findAfterDay = function(day, month, year){
+var findAfterDay = function (day, month, year) {
     var tempAfter = "";
-     if(day < calculatingDaysOfMonth(month, year)){
+    if (day < calculatingDaysOfMonth(month, year)) {
         day++;
-    } else if(month<12){
-        day=1;
+    } else if (month < 12) {
+        day = 1;
         month++;
-    }else{
-        day= month =1;
+    } else {
+        day = month = 1;
         year++
     }
     return tempAfter = day + '/' + month + '/' + year
@@ -73,18 +71,18 @@ var findAfterDay = function(day, month, year){
 
 
 var resultCheck = document.getElementById('resultCheck');
-document.getElementById('btnCheck').addEventListener('click', function(){
+document.getElementById('btnCheck').addEventListener('click', function () {
     var ipDay = parseInt(document.getElementById('ipDay').value);
-    var ipMonth = parseInt (document.getElementById('ipMonth').value);
+    var ipMonth = parseInt(document.getElementById('ipMonth').value);
     var ipYear = parseInt(document.getElementById('ipYear').value);
     var resultPre = findPreviousDay(ipDay, ipMonth, ipYear);
     var resultAfter = findAfterDay(ipDay, ipMonth, ipYear);
-    
+
     resultCheck.style.display = 'block';
-    resultCheck.innerHTML = 'Tomorrow is: ' + resultAfter + '<br> ' + 'Yesterday is: ' + resultPre; 
-    
+    resultCheck.innerHTML = 'Tomorrow is: ' + resultAfter + '<br> ' + 'Yesterday is: ' + resultPre;
+
 });
-    
+
 /**
  * Bài 2:
  *- Input: month, year
@@ -105,10 +103,10 @@ document.getElementById('btnCheck').addEventListener('click', function(){
  * 
  */
 var resultCount = document.getElementById('resultCount');
-document.getElementById('btnCount').addEventListener('click', function(){
+document.getElementById('btnCount').addEventListener('click', function () {
     var ipMonth2 = parseInt(document.getElementById('ipMonth2').value);
-    var ipYear2 =  parseInt(document.getElementById('ipYear2').value);
- 
+    var ipYear2 = parseInt(document.getElementById('ipYear2').value);
+
     var resultDay = calculatingDaysOfMonth(ipMonth2, ipYear2);
     //hàm calculatingDaysOfMonth nhận vào 2 tham số kiểu int, còn document.getElementById('ipMonth2').value là string
     //nên khi truyền vô 2 ts ipMonth vs ipYear kiêu string bị lỗi
@@ -121,84 +119,134 @@ document.getElementById('btnCount').addEventListener('click', function(){
 /**
  * Bài 3:
  */
- var inRaCachDoc = function(soCoBaChuSo){
-    var tmp;
-    if(soCoBaChuSo <100 || soCoBaChuSo > 999){
-        alert('Vui lòng nhập vào số có 3 chữ số!');
-    }else{
-        var donVi = Math.round(soCoBaChuSo % 10);
-        soCoBaChuSo/= 10;
-        var chuc = Math.round(soCoBaChuSo%10);
-        var tram =Math.round(soCoBaChuSo/10);
-        if(tram == 1){
-            console.log('Một');
-        } else if(tram == 2){
-            console.log('Hai');
-        } else if(tram == 3){
-            console.log('Ba')
-        } else if(tram == 4){
-            console.log('Bốn');
-        } else if(tram == 5){
-            console.log('Năm')
-        } else if(tram == 6){
-            console.log('Sáu');
-        } else if(tram == 7){
-            console.log('Bảy');
-        } else if(tram == 8){
-            console.log('Tám');
-        } else if(tram == 9){
-            console.log('Chín');
-        }
-        console.log('Trăm');
-
-        if(chuc == 2){
-            console.log('Hai');
-        } else if(chuc == 3){
-            console.log('Ba');
-        } else if(chuc == 4){
-            console.log('Bốn');
-        } else if(chuc == 5){
-            console.log('Năm');
-        } else if(chuc == 6){
-            console.log('Sáu');
-        } else if(chuc == 7){
-            console.log('Bảy');
-        }else if(chuc == 8){
-            console.log('Tám');
-        } else if(chuc == 9){
-            console.log('Chín');
-        }
-        console.log('Mươi');
-
-        if(donVi == 1){
-            console.log('Một');
-        } else if(donVi == 2){
-            console.log('Hai');
-        } else if(donVi == 3){
-            console.log('Ba');
-        } else if(donVi == 4){
-            console.log('Bốn');
-        } else if(donVi == 5){
-            console.log('Năm');
-        } else if(donVi == 6){
-            console.log('Sáu');
-        } else if(donVi == 7){
-            console.log('Bảy');
-        } else if(donVi == 8){
-            console.log('Tám');
-        }else if(donVi == 9){
-            console.log('Chín');
-        } 
-    } 
-    return tmp = tram + ' Trăm ' + chuc + ' Mươi ' + donVi;
-
+var hangTram = function (soCoBaChuSo) { 
+    var tmpTram = '';
+    soCoBaChuSo /= 10;
+    switch (Math.round(soCoBaChuSo / 10)) {
+        case 0:
+            tmpTram = 'Không Trăm';
+            break;
+        case 1:
+            tmpTram = 'Một Trăm';
+            break;
+        case 2:
+            tmpTram = 'Hai Trăm';
+            break;
+        case 3:
+            tmpTram = 'Ba Trăm';
+            break;
+        case 4:
+            tmpTram = 'Bốn Trăm';
+            break;
+        case 5:
+            tmpTram = 'Năm Trăm';
+            break;
+        case 6:
+            tmpTram = 'Sáu Trăm';
+            break;
+        case 7:
+            tmpTram = 'Bảy Trăm';
+            break;
+        case 8:
+            tmpTram = 'Tám Trăm';
+            break;
+        case 9:
+            tmpTram = 'Chín Trăm';
+            break;
+    }
+    return tmpTram;
 };
 
-document.getElementById('btnRead').addEventListener('click', function(){
+var hangChuc = function (soCoBaChuSo) {
+    var tmpChuc = '';
+    soCoBaChuSo /= 10;
+    switch (Math.round(soCoBaChuSo % 10)) {
+        case 0:
+            tmpChuc = 'Ling';
+            break;
+        case 1:
+           tmpChuc = 'Mươi';
+            break;
+        case 2:
+            tmpChuc = 'Hai Mươi';
+            break;
+        case 3:
+            tmpChuc = 'Ba Mươi';
+            break;
+        case 4:
+            tmpChuc = 'Bốn Mươi';
+            break;
+        case 5:
+            tmpChuc = 'Năm Mươi';
+            break;
+        case 6:
+            tmpChuc = 'Sáu Mươi';
+            break;
+        case 7:
+            tmpChuc = 'Bảy Mươi';
+            break;
+        case 8:
+            tmpChuc = 'Tám Mươi';
+            break;
+        case 9:
+            tmpChuc = 'Chín Mươi';
+            break;
+    }
+    return tmpChuc;
+};
+
+var hangDonVi = function (soCoBaChuSo) {
+    var tmpDonVi = '';
+    switch (Math.round(soCoBaChuSo % 10)) {
+        case 0:
+            tmpDonVi = 'Không';
+            break;
+        case 1:
+            tmpDonVi = 'Một';
+            break;
+        case 2:
+            tmpDonVi = 'Hai';
+            break;
+        case 3:
+            tmpDonVi = 'Ba';
+            break;
+        case 4:
+            tmpDonVi = 'Bốn';
+            break;
+        case 5:
+            tmpDonVi = 'Năm';
+            break;
+        case 6:
+            tmpDonVi = 'Sáu';
+            break;
+        case 7:
+            tmpDonVi = 'Bảy';
+            break;
+        case 8:
+            tmpDonVi = 'Tám';
+            break;
+        case 9:
+            tmpDonVi = 'Chín';
+            break;
+    }
+    return tmpDonVi;
+};
+
+document.getElementById('btnRead').addEventListener('click', function () {
     var ipNumber = document.getElementById('ipNumber').value;
-    var result = inRaCachDoc(ipNumber);
-    
+    var tram = hangTram(ipNumber);
+    var chuc = hangChuc(ipNumber);
+    var donVi = hangDonVi(ipNumber);
+    if(ipNumber<100 && ipNumber>999){
+        alert('Nhập vào số có 3 chữ số!');
+    }else{
     var resultRead = document.getElementById('resultRead');
     resultRead.style.display = 'block';
-    resultRead.innerHTML = ipNumber + 'đọc là: ' + result;
-})
+    resultRead.innerHTML = ipNumber + ' đọc là: ' + ' ' +tram + ' ' +chuc + ' ' +donVi;
+    }
+});
+
+/**
+ *
+ *
+ */
